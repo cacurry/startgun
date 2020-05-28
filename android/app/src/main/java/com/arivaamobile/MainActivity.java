b8755816672727ab43ff64541a4cb01e3b09d4b5
Inderdeep Singh <inderdeep.singh@laxaar.com>
2020-05-28 18:02:36 +0530

    IOS and Android Build Fixes

--- android/app/src/main/java/com/arivaamobile/MainActivity.java
+++ android/app/src/main/java/com/arivaamobile/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends ReactActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
-    SplashScreen.show(this); // here
+
     super.onCreate(savedInstanceState);
-  }
+SplashScreen.show(this, R.style.SplashScreenTheme);  }
 }
